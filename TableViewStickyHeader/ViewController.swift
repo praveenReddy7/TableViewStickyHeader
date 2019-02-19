4814473de8978100540b2ae4caf362f9a6772088
Apple <praveen@apples-macbook-pro.local>
2019-02-19 12:44:16 +0530

    Initial Commit

---
+++ TableViewStickyHeader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewStickyHeader
+//
+//  Created by praveen on 2/19/19.
+//  Copyright © 2019 DemoApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
